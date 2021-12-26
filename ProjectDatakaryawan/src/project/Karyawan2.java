092ca6acc5f73e01e5df0bbe556d310a7edfdc99
Aqil Sadik <dmadaaqil@gmail.com>
2021-12-26 21:50:31 +0700

    Create Karyawan2.java

---
+++ ProjectDatakaryawan/src/project/Karyawan2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author User
+ */
+public interface Karyawan2 {
+    public void TambahData(Karyawan dataKaryawan);
+    public void HapusData(String kodeKaryawan);
+    public void CariData(String kodeKaryawan);
+    public void LihatData();
+}
